3a480cae184c97b19b20df335d433a5e402f40fd
DEVRED Paul <devred@et.intechinfo.fr>
2023-06-15 10:36:11 +0200

    copie de indev

---
+++ BotApi/src/main/java/com/example/BotApi/BotApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.BotApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BotApiApplication.class, args);
+
+	}
+}
